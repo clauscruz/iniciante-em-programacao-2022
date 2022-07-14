b2617c8ad4a0fcb8c1ff5466aa72ec39e8d2c8b0
Claudio Barros <claudiocruzbarros@gmail.com>
2022-07-14 19:47:40 -0300

    alteracoes que fiz implemantando selecionar area

--- jogo-velha.js
+++ jogo-velha.js
@@ -1,10 +1,25 @@
 const X = "X";
 const O = "O";
+let valor = X;
+const quadradoJogo = document.querySelector(".quadrado-jogo");
 
 function reiniciarJogo() {
     
 }
 
 function selecionarArea(posicaoLinha, posicaoColuna) {
+     desenharSimbolo(valor, posicaoLinha, posicaoColuna);
+    if (valor ==X) {
+        valor = O
+        
+    }
+     else {
+        valor = X
 
+     }
+
+    
+     marcarJogadorAtivo(valor);
+
+     exibirResultado("");
 }
